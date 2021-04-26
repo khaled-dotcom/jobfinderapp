889deb9a066380a7f23d0ac828916c94133356be
khaled-dotcom <68504619+khaled-dotcom@users.noreply.github.com>
2021-04-26 20:28:41 +0100

    Finished

--- lib/screens/jobDetail_screen/widgets/job_status.dart
+++ lib/screens/jobDetail_screen/widgets/job_status.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class JobStatus extends StatefulWidget {
   @override
   _JobStatusState createState() => _JobStatusState();
@@ -11,30 +12,46 @@ class _JobStatusState extends State<JobStatus> {
       height: 70,
       padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
-          border: Border.all(width: 2.0,color: Colors.black26),
-          borderRadius: BorderRadius.circular(10.0)
-      ),
+          border: Border.all(width: 2.0, color: Colors.black26),
+          borderRadius: BorderRadius.circular(10.0)),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Flexible(
             child: Container(
-              child: Image.asset("assets/icons/members.png",color: Colors.deepOrange,),
+              child: Image.asset(
+                "assets/icons/members.png",
+                color: Colors.deepOrange,
+              ),
             ),
           ),
-          Text("5 Member",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
-          Container(height: 40,width: 2,color:Colors.black26),
+          Text(
+            "5 Member",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          Container(height: 38, width: 2, color: Colors.black26),
           Flexible(
             child: Container(
-              child: Image.asset("assets/icons/like.png",color: Colors.deepOrange,),
+              child: Image.asset(
+                "assets/icons/like.png",
+                color: Colors.deepOrange,
+              ),
             ),
           ),
-          Text("40k Likes",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
-          Container(height: 40,width: 2,color:Colors.black26),
-          Flexible(child: Icon(Icons.location_on_outlined,color: Colors.deepOrange,)),
-          Text("Yogyakarta",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
-
-
+          Text(
+            "40k Likes",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          Container(height: 38, width: 2, color: Colors.black26),
+          Flexible(
+              child: Icon(
+            Icons.location_on_outlined,
+            color: Colors.deepOrange,
+          )),
+          Text(
+            "Yogyakarta",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
         ],
       ),
     );
