ba788a433545ec290a451ec6aff002640abb07b8
khaled-dotcom <68504619+khaled-dotcom@users.noreply.github.com>
2021-04-26 21:54:09 +0100

    job catagory updated

--- lib/screens/home_screen/widgets/job_catagory.dart
+++ lib/screens/home_screen/widgets/job_catagory.dart
@@ -16,29 +16,31 @@ class _JobCatagoryState extends State<JobCatagory> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 30.0,
+      height: 32.0,
       child: ListView.separated(
           scrollDirection: Axis.horizontal,
           itemCount: listCatagory.length,
           separatorBuilder: (_, __) => SizedBox(width: 15.0),
           itemBuilder: (BuildContext context, int index) {
-            return index == 0
-                ? Container(
-                    padding: EdgeInsets.all(8.0),
+            if (index == 0) {
+              return Container(
+                    padding: EdgeInsets.all(7.0),
                     decoration: BoxDecoration(
                         color: Colors.deepOrange,
-                        borderRadius: BorderRadius.circular(15.0)),
+                        borderRadius: BorderRadius.circular(13.0)),
                     child: Text(listCatagory[index],
                         style: TextStyle(color: Colors.white, fontSize: 15.0)),
-                  )
-                : Container(
+                  );
+            } else {
+              return Container(
                     padding: EdgeInsets.all(7.0),
                     decoration: BoxDecoration(
                         color: Colors.brown[50],
-                        borderRadius: BorderRadius.circular(10.0)),
+                        borderRadius: BorderRadius.circular(13.0)),
                     child: Text(listCatagory[index],
                         style: TextStyle(color: Colors.grey, fontSize: 15.0)),
                   );
+            }
           }),
     );
   }
