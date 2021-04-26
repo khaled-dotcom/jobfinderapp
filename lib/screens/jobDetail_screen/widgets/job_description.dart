8e5fbe28adc4b22f91ae38e9ccc434cc3fb330dc
khaled-dotcom <68504619+khaled-dotcom@users.noreply.github.com>
2021-04-26 20:14:10 +0100

    Finished

--- lib/screens/jobDetail_screen/widgets/job_description.dart
+++ lib/screens/jobDetail_screen/widgets/job_description.dart
@@ -8,18 +8,31 @@ class JobDescription extends StatefulWidget {
 class _JobDescriptionState extends State<JobDescription> {
   @override
   Widget build(BuildContext context) {
-    return  Container(
-        margin: EdgeInsets.symmetric(vertical: 20.0),
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-    Text("Job Description",style: TextStyle(fontSize: 20.0,color: Colors.black,fontWeight: FontWeight.bold),),
-    SizedBox(height: 17.0,),
-    Text("Your job is to design an application or website that is as attractive and creative as possible."
-    "here you work fulltime and have to be diligent and beable to use animation to make it more interesting",
-      style: TextStyle(fontSize: 15.0,color: Colors.black54,letterSpacing:0.5,height: 1.5),),
-      ]
-    )
-    );
+    return Container(
+        margin: EdgeInsets.symmetric(vertical: 18.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          //description title
+          Text(
+            "Job Description",
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 17.0,
+          ),
+
+          // description
+          Text(
+            "Your job is to design an application or website that is as attractive and creative as possible."
+            "here you work fulltime and have to be diligent and beable to use animation to make it more interesting",
+            style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.black54,
+                letterSpacing: 0.5,
+                height: 1.5),
+          ),
+        ]));
   }
 }
